a7c76a29b9a0a056c324aec4e1c96834b0ae2542
Kamil Łęga <lega-kamil@wp.pl>
2022-04-01 10:02:22 +0200

    dodanie algorytmu liczb pierwszych oraz obliczenia nww i nwd

---
+++ algorytm liczb pierwszych/algorytm liczb pierwszych/algorytm liczb pierwszych.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int lp = 0;
+    int p = 2;
+    
+    cout << "podaj ilosc liczb pierwszych: ";
+    cin >> n;
+
+    while (lp < n) {
+        for (int d = 2; d <= p-1; d++) {
+            if ( p % d == 0) {
+                p = p + 1;
+            }   
+        }
+        cout << p + " jest pierwsze!";
+        lp = lp + 1;
+        p = p + 1;
+    }
+}
---
+++ nww/nww/nww.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int iloczyn = 0;
+    int nww = 0;
+    cout << "podaj a: ";
+    cin >> a;
+    cout << "podaj b: ";
+    cin >> b;
+
+    iloczyn = a * b;
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+        nww = iloczyn / a;
+    }
+    cout << "najmniejsza wspolna wielokrotnosc: ";
+    cout << nww;
+    cout << endl;
+    cout << "najwiekszy wspolny dzielnik: ";
+    cout << a;
+
+}
+
