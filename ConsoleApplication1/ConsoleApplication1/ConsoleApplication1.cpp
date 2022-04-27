6830809f6d26237fb08739c112272841b012f096
Lega-kamil <lega-kamil@wp.pl>
2022-04-27 10:50:57 +0200

    Ciag Fibonacciego

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void ciag(int n)
+{
+	int f0, f1, f;
+	f0 = 0;
+	f1 = 1;
+
+	for (int i = 0; i < n; i++) {
+		if (i > 1) 
+		{
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else 
+		{
+			f = i;
+		}
+		cout << f;
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "podaj ilość liczb ciagu fibonacciego: ";
+	cin >> n;
+	ciag(n);
+
+}
+
